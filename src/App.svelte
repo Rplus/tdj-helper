dd5f2b5efba0eed2d1745033c7715863656e3e6e
Rplus <rplus.tw@gmail.com>
2022-03-21 17:36:19 +0800

    fix: arrow char

--- src/App.svelte
+++ src/App.svelte
@@ -135,12 +135,12 @@
 
 
 <h1>
-  <a href="./naihe/" title="三途川選擇">⏴</a>
+  <a href="./naihe/" title="三途川選擇">↤</a>
   <ruby>
     絕品魂石 評估
     <rt style="color: #0003">天地劫M</rt>
   </ruby>
-  <a href="./weapon/" title="go to next">⏵</a>
+  <a href="./weapon/" title="go to next">↦</a>
 </h1>
 
 <form class="main"
