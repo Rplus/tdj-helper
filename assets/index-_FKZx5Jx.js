aa6ebe082b09c269aa93d5cf2cf13fc8372d38db
Rplus <rplus.tw@gmail.com>
2023-12-16 16:25:11 +0800

    Updates

---
+++ assets/index-_FKZx5Jx.js
@@ -0,0 +1,77 @@
+import"./modulepreload-polyfill-9p4a8sJU.js";let o=[{name:"魔羊掛角",member:`靈鼩 —— — — 寧采臣
+— 相桓子 —— 劍聖
+— —— 李盈鳳 ——`},{name:"魔羊掛角",member:`劍平 —— — — 燕明蓉
+— 鮮于超 — 封寒月
+— —— 紫楓 ——`},{name:"魔羊掛角",member:`殷劍平 —— — — 封寒月
+— 夏侯儀 —— 璇璣 —
+— ——— 葛雲衣 ——`},{name:"魔羊掛角",member:`雲衣宮主 —— — — 司徒纓
+— 靈鼩 ——— 諸葛艾
+— —— 古倫德 ——`},{name:"魔羊掛角",member:`封寒月 —— — — 葛雲衣
+— 璇璣 —— 曹沁
+— —— 夏侯儀 ——`},{name:"魔羊掛角",member:`奚歌 —— — — 赤練
+— 劍邪 —— 林月如
+— —— 曹沁 ——`},{name:"舊域迷陣",member:`召祐 鮮于超 —
+——— ——— 鄲陰
+黎幽 —— 玄羽`},{name:"舊域迷陣",member:`任斷離 玄羽 ——
+———— ———— 九陰
+上官遠 —— 高皇君`},{name:"舊域迷陣",member:`靈鼩 奚歌 ——
+——— ——— 曹沁
+赤練 —— 林月如`},{name:"舊域迷陣",member:`冰璃 陰歙 —
+———— ———— 陽寰
+夏侯儀 —— 霍雍`},{name:"舊域迷陣",member:`古倫德 阿良 ——
+———— ———— 璇璣
+燕明蓉 —— 葛雲衣`},{name:"險境勘探",member:"殷劍平 冰璃 夏侯儀 依依 葛雲衣"},{name:"險境勘探",member:"太師 封寒月 夏侯儀 銀瑪 冰璃"},{name:"險境勘探",member:"應奉仁 劍邪 殷劍平 高皇君 殷無邪"},{name:"險境勘探",member:"于小雪 陰歙 趙靈兒 冰璃 封寒月"},{name:"險境勘探",member:"韓千秀 春蘭 雲襄 殷劍平 舞蝶"},{name:"險境勘探",member:"葛雲衣 冰璃 夏侯儀 璇璣 依依"},{name:"險境勘探",member:"璇璣 夏侯儀 冰璃 依依 葛雲衣"},{name:"惡魔試煉",member:`—— 阿良 夏侯儀
+殷劍平 BOSS
+—— 冰璃 璇璣`},{name:"惡魔試煉",member:`—— 劍邪 高戚
+鄲陰 BOSS
+—— 方芸 韓無砂`},{name:"惡魔試煉",member:`—— 聶小倩 雲衣宮主
+雲襄 BOSS
+—— 霍雍 魔夏`},{name:"惡魔試煉",member:`—— 殷劍平 曹沁
+依依 BOSS
+—— 阿良 冰璃`},{name:"惡魔試煉",member:`—— 燕赤霞 九陰
+林月如 BOSS
+—— 方芸 紫楓`},{name:"惡魔試煉",member:`—— 劍邪 黑龍
+奚歌 BOSS
+—— 魔夏 霍雍`},{name:"突擊連環",member:"阿良 無邪 依依 黎幽 虞兮"},{name:"突擊連環",member:"任斷離 方芸 銀瑪 奚歌 劍聖"},{name:"突擊連環",member:"葛雲衣 曹沁 依依 夏侯儀 韓無砂"},{name:"突擊連環",member:"冰璃 阿良 韓無砂 夏侯儀 曹沁"},{name:"突擊連環",member:"璇璣 阿良 劍平 夏侯儀 依依"},{name:"突擊連環",member:"曹沁 韓無砂 葛雲衣 封寒月 阿良"},{name:"江湖紛爭",member:"璇璣 古倫德 封鈴笙 冰璃 夏侯儀"},{name:"江湖紛爭",member:"紫蘊 高皇君 朱繯 葛雲衣 白莞"},{name:"江湖紛爭",member:"寧采臣 燕赤霞 韓無砂 聶小倩 朧妖"},{name:"江湖紛爭",member:"趙靈兒 李逍遙 太師 林月如 于小雪"},{name:"江湖紛爭",member:"諸葛艾 沒錯 虞兮 李逍遙 封鈴笙"},{name:"江湖紛爭",member:"封鈴笙 雲衣宮主 秦惟剛 春蘭 燕明蓉"},{name:"古神祈願",member:`—— 諸葛艾 ——
+紫蘊 ——— 朱繯
+葛雲衣 —— 高皇君`},{name:"古神祈願",member:`—— 阿良 ——
+冰璃 —— 璇璣
+曹沁 —— 封寒月`},{name:"古神祈願",member:`—— 黑龍 ——
+九陰 —— 鮮于超
+高戚 —— 上官遠`},{name:"古神祈願",member:`—— 韓無砂 —
+璇璣 —— 曹沁
+劍平 —— 依依`},{name:"古神祈願",member:`—— 鮮于超 ——
+任斷離 —— 奚歌
+劍邪 ——— 黎幽`},{name:"古神祈願",member:`—— 劍聖 ——
+銀瑪 —— 鄲陰
+魔夏 —— 阿良`}];o.forEach(e=>{e.member=p(e.member)});console.log(o[0].member);console.log(p(o[0].member));const d=document.getElementById("form-box"),i=document.getElementById("output"),f=document.getElementById("link");let s,b=[!1,!1,!1];window.onload=async function(e){var n,t;s=[...new Set(o.map(l=>l.name))],g(),location.search&&(k(o),i.rows=25),b.forEach(l=>v(l)),(n=document.getElementById("copy"))==null||n.addEventListener("click",y),(t=document.getElementById("resize"))==null||t.addEventListener("click",()=>{i.style="",i.rows=i.rows>20?5:25})};function y(){navigator.clipboard.writeText(i.value)}function g(){let e=document.createElement("style");e.textContent=s.map(n=>`form[data-click="${n}"] .lineups label[data-type="${n}"] { display: flex;}`).join(""),d.append(e)}function v(e){var r,c;if(document.querySelectorAll("form").length>2)return;let n=Math.random()+ +new Date,t=s.map(a=>`<label class="flex" data-type>
+        <input data-click="types" type="radio"
+          name="${n}"
+          value="${a}">
+        <span>${a}</span>
+      </label>`).join(""),l=o.map(a=>`<label data-type="${a.name}">
+        <input type="radio" data-click="members"
+          name="lineups-${n}"
+          value="${a.member}">
+        <div class="member-content">${a.member.trim()}</div>
+      </label>`).join(""),m=document.createElement("form");m.addEventListener("input",a=>{a.target.dataset.click==="types"&&(m.dataset.click=a.target.value,m.style=`--click: '${a.target.value}'`)}),m.addEventListener("change",S),m.innerHTML=`
+    <details open class="mb-1">
+      <summary class="mb-1"></summary>
+      <div class="flex ml-1 flex-wrap">
+        <div class="flex-left">
+          <strong>關卡</strong>
+          <div>${t}</div>
+        </div>
+        <div class="ml-1">
+          <strong>陣容</strong>
+          <div class="lineups" data-type="">${l}</div>
+        </div>
+      </div>
+    </details>`,d.append(m),e&&((r=m.querySelector(`input[data-click="types"][value="${e.name}"]`))==null||r.click(),(c=[...m.querySelectorAll('input[data-click="members"]')].find(a=>a.value===e.member))==null||c.click())}function S(){let e=[...document.querySelectorAll("form")],n=[];i.textContent=e.map((t,l)=>{var c;let m=t.dataset.click,r=((c=t.querySelector(`label[data-type="${m}"] input[data-click="members"]:checked`))==null?void 0:c.value)||"";return m&&r&&(n[l]={type:m,value:r}),`#${l+1} ${m||"?"}:
+
+${r||"?"}`}).join(`
+
+
+`),$(n)}function $(e){let n=e.map((t,l)=>{let m=t.value.replace(/[—＿]+/g,"").trim().replace(/\s+/g,"+");return`${l+1}=${t.type}:${m}`}).join("&");f.href=`?${n}`}function k(e){let n=new URLSearchParams(location.search.replace(/＿+\+/g,""));b=[n.get("1"),n.get("2"),n.get("3")].map(t=>{if(!t)return null;let[l,m]=t.split(":");return e.find(r=>r.name.includes(l)?m==null?void 0:m.split(" ").every(c=>r.member.includes(c)):!1)})}function p(e){let n=[];return e=e.replace(/[—-]+/g,"＿"),e.split(`
+`).map(m=>{let r=m.trim().split(/\s+/g);return r.forEach((c,a)=>{let u=w(c);(u>n[a]||!n[a])&&(n[a]=u)}),r}).map(m=>m.map((r,c)=>h(r,n[c])).join(" ")).join(`
+`)}function h(e,n){return e.replace(/[—-]+/g,"＿").padEnd(n,"＿")}function E(e){return/[^\x00-\x7F]/.test(e)}function w(e){return e.split("").reduce((n,t)=>n+(E(t)?1:.5),0)}
