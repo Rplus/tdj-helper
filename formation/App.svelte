8a1e1a32fcc17f2347802f2d7b2ed22fecb940e6
Rplus <rplus.tw@gmail.com>
2023-12-16 19:30:33 +0800

    feat: add formation page

---
+++ formation/App.svelte
@@ -0,0 +1,203 @@
+<script>
+	import data from '/roles/data.json';
+	import Item from './Item.svelte';
+
+	let filters = init_filters();
+	let search_kwd = '';
+
+	$: filetr_style  = `<style>${gen_filter_style(filters)}</style>`;
+	$: search_style  = `<style>${gen_search_style(search_kwd)}</style>`;
+
+	function gen_filter_style(_filters) {
+		let checked = _filters
+			.filter(f => f.checked)
+			.map(item => item.key);
+
+		if (!checked.length) {
+			return '';
+		}
+		let selectors = checked.map(i => `:not(.mem-${i})`).join('');
+		return `.row${selectors} { display: none; }`;
+	}
+
+	function gen_search_style(kwd = '') {
+		if (!kwd) {
+			return '';
+		}
+
+		console.log({kwd});
+
+		return `.row.row:not([data-info*="${kwd.trim()}"]) { display: none; }`;
+	}
+
+	function gen_members_class(members) {
+		return members.map(m => `mem-${m}`).join(' ');
+	}
+
+	function init_filters() {
+		return [
+			'炎', '雷', '冰', '光', '暗', '幽',
+			'俠客', '鐵衛', '祝由', '御風', '羽士', '咒師', '鬥將',
+		].map(key => ({
+			key,
+			checked: false,
+		}));
+	}
+
+	function reset_filter() {
+		filters = init_filters();
+	}
+</script>
+
+
+
+<div class="workspace">
+	<div class="style">
+		{@html filetr_style}
+		{@html search_style}
+	</div>
+
+	<form class="filters" on:reset|preventDefault={reset_filter}>
+		<div class="hr">
+			🔍 + 聯集(⋃)
+			<input type="reset" />
+		</div>
+
+		{#each filters as filter, index}
+			{#if index === 6 }
+				<br>
+			{/if}
+			<label>
+				<input type="checkbox" bind:checked={filter.checked}>
+				{filter.key}
+			</label>
+		{/each}
+		<div class="search-box">
+			<input type="search" bind:value={search_kwd}
+				placeholder="過濾說明" />
+		</div>
+	</form>
+	<hr>
+
+	{#each data.formations as formation (formation.name)}
+		<div class="row flex {gen_members_class(formation.members)}"
+			data-info={formation.info}
+		>
+			<div class="members flex">
+				<Item
+					title={formation.name}
+					img={formation.avatar.src}
+					link={formation.url}
+				/>
+
+				{#each formation.members as member}
+					<Item
+						title={member}
+					/>
+				{/each}
+			</div>
+
+			<div class="info">{formation.info}</div>
+		</div>
+
+	{/each}
+
+	<hr>
+
+	<h1>
+    <a href="../" title="back">../</a>
+		天地劫M 戰陣圖鑑
+	</h1>
+
+	<footer class="footer">
+		資料來源：<a href="https://wiki.biligame.com/tdj/" target="_biliwiki">https://wiki.biligame.com/tdj/</a>
+	</footer>
+</div>
+
+
+
+<style global>
+.workspace {
+	padding: .5em;
+	justify-content: center;
+	min-height: 100vh;
+	margin: 0 auto;
+	max-width: 50em;
+	align-content: stretch;
+}
+
+.row {
+	margin-bottom: 1.5em;
+
+	@media (max-width: 700px) {
+		flex-wrap: wrap;
+	}
+
+	& > div:first-child {
+		margin-right: 1em;
+	}
+}
+
+.members {
+	gap: .5em;
+	margin-bottom: .5em;
+	text-align: center;
+}
+
+.info {
+	max-width: 22em;
+	margin-bottom: 1em;
+	align-self: flex-start;
+	padding-bottom: 1em;
+
+	@media (max-width: 700px) {
+		max-width: unset;
+		padding-left: .75em;
+		padding-right: 1.5em;
+		margin-left: .65em;
+		border-left: .4em solid #0001;
+	}
+}
+
+.hr {
+	margin-bottom: 1em;
+}
+
+.filters {
+	position: relative;
+	text-align: center;
+}
+.filters label {
+	display: inline-flex;
+	align-items: center;
+	gap: .25em;
+	margin: .25em;
+	user-select: none;
+}
+
+.search-box {
+	position: absolute;
+	right: 0;
+
+	& input {
+		padding: 0.25em;
+	}
+}
+
+input[type="checkbox"] {
+	margin: 0;
+	width: 1em;
+	height: 1em;
+}
+
+hr {
+	margin: 2em 0;
+}
+
+h1 {
+	text-align: center;
+}
+input:active {
+	background-color: #ffc9;
+}
+</style>
---
+++ formation/Item.svelte
@@ -0,0 +1,68 @@
+<script>
+	export let title;
+	export let img = '';
+	export let link = '';
+
+	import data from '/roles/data.json';
+
+	let role = data.roles.find(role => role.name === title);
+	if (role && !img) {
+		img = role.avatar.src;
+		link = role.link;
+	}
+
+</script>
+
+
+
+<div class="member">
+	<div class="avatar">
+		{#if img}
+			<img decoding="async" loading="lazy"
+				src={img}
+				alt={title}
+				width="60"
+				height="60"
+			/>
+		{/if}
+	</div>
+	{#if link}
+		<a href={link} target="_biliwiki">
+			{title}
+		</a>
+	{:else}
+		<span>
+			{title}
+		</span>
+	{/if}
+</div>
+
+
+
+<style>
+.member {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+.avatar {
+	width: 90px;
+	height: 90px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: #9993;
+
+	@media (max-width: 900px) {
+		width: 60px;
+		height: 60px;
+	}
+}
+
+.avatar img {
+	max-width: 90%;
+	max-height: 90%;
+}
+</style>
