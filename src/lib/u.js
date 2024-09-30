9f50c3c4d15e4b8e933051554c294739a7b032c5
Rplus <rplus.tw@gmail.com>
2024-10-01 04:33:10 +0800

    feat: fallback to origin image path
    ref: https://images.weserv.nl/docs/format.html#default-image

--- src/lib/u.js
+++ src/lib/u.js
@@ -32,7 +32,7 @@ export function resize_img(url = '', size = 128) {
 	if (!url) {
 		return '';
 	}
-	return `https://wsrv.nl/?&w=${size}&h=${size}&we&il&output=webp&url=${url}`;
+	return `https://wsrv.nl/?&w=${size}&h=${size}&we&il&output=webp&url=${url}&default=1`;
 }
 
 export function resize_imgs(url = '', sizes = [128]) {
