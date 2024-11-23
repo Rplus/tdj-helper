ddc761cece2b1cf1f190470fc2827d834b06f21e
Rplus <rplus.tw@gmail.com>
2024-11-24 00:15:38 +0800

    fix: sucks formating for strategy data

--- src/routes/strategy/strategy.js
+++ src/routes/strategy/strategy.js
@@ -2,7 +2,7 @@ import data from '$lib/data/strategy.min.json';
 
 data.forEach((i) => {
 	i.desc_html = i.desc.replace(
-		/.+所有我方上陣角色物攻，物防，法攻，法防提高(\d+%)[。，](.+)?$/gm,
+		/.+所有我方上陣角色物攻，物防，法攻，法防提[高升](\d+%)[。，](.+)?$/gm,
 		`攻防+$1\n$2`,
 	);
 });
