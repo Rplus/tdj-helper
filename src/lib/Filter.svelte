59f6b655fc3192d51f3571b3b1957b4863c20dbf
Rplus <rplus.tw@gmail.com>
2026-05-31 12:14:38 +0800

    feat: refactor with ai (chatgpt)

--- src/lib/Filter.svelte
+++ src/lib/Filter.svelte
@@ -1,30 +1,14 @@
 <script>
-export let filter_cates = []; // filters array
-export let item_class = '.item'; // string
+export let filter_cates = [];
+export let item_class = '.item';
 export let placeholder = '過濾名稱';
 export let gen_selector = (prop, value, multi) => `[data-${prop}${multi ? '*' : ''}="${value}"]`;
 export let search_cb = null;
 
-import { browser } from '$app/environment';
-import { replaceState } from '$app/navigation';
-import { onMount } from 'svelte';
+import { page } from '$app/stores';
+import { goto } from '$app/navigation';
 import { writable } from 'svelte/store';
 import { saveItem, getItem } from '$lib/u.js';
-
-const STORAGE_KEY = 'filter-toggle-status';
-
-const details_store = writable(getItem(STORAGE_KEY) || {});
-
-details_store.subscribe((obj) => {
-	saveItem({ key: STORAGE_KEY, value: obj });
-});
-
-function handle_toggle(io_key, is_open) {
-	details_store.update(obj => {
-		obj[io_key] = is_open;
-		return obj;
-	});
-}
 
 // ### filter_cates sample
 // # TODO: values contain (value, title)?
@@ -46,172 +30,224 @@ function handle_toggle(io_key, is_open) {
 // 	},
 // ];
 
-const query_param = 'q';
+const STORAGE_KEY = 'filter-toggle-status';
+const QUERY_PARAM = 'q';
 
-let filters = create_filters(new Set(get_qs()));
-let input_value = (browser && new URLSearchParams(location.search).get(query_param)) || '';
-let search_kwd = input_value ||'';
-let is_composing = false;
+const details_store = writable(getItem(STORAGE_KEY) || {});
 
-$: filter_style = `${gen_filter_style(filters)}`;
-$: search_style = search_kwd ? `${gen_search_style(search_kwd)}` : '';
-
-let loaded = false;
-
-onMount(() => {
-	loaded = true;
+details_store.subscribe((obj) => {
+	saveItem({
+		key: STORAGE_KEY,
+		value: obj,
+	});
 });
 
-$: {
-	update_state('?' + gen_qs(filters, search_kwd));
+function handle_toggle(io_key, is_open) {
+	details_store.update((obj) => ({
+		...obj,
+		[io_key]: is_open,
+	}));
 }
 
-function update_state(qs = '') {
-	if (loaded) {
-		replaceState(qs);
-	}
+let is_composing = false;
+
+$: search_params = $page.url.searchParams;
+
+$: checked_set = get_checked_set(search_params);
+
+$: search_kwd = search_params.get(QUERY_PARAM) || '';
+
+$: filters = create_filters(checked_set, search_params);
+
+$: filter_style = gen_filter_style(filters, search_params);
+
+$: search_style = search_kwd
+	? gen_search_style(search_kwd)
+	: '';
+
+let prev_search_kwd;
+
+$: if (
+	!is_composing
+	&& search_cb
+	&& search_kwd !== prev_search_kwd
+) {
+	prev_search_kwd = search_kwd;
+	search_cb(search_kwd);
 }
 
-function gen_qs(filters, search_kwd = '') {
-	let qs = filters
-		.map((cate) => {
-			let keys = cate.options
-				.map((option) => {
-					return option.checked ? option.key : null;
-				})
-				.filter(Boolean)
-				.join('|');
-
-			return keys.length ? `${cate.prop}=${keys}` : null;
-		})
-		.filter(Boolean)
-		.join('&');
-
-	if (search_kwd) {
-		qs += `&${query_param}=${search_kwd}`;
-	}
-
-	return qs;
-}
-
-// $: console.log({filters});
-
-$: {
-	if (!is_composing && search_kwd !== input_value) {
-		search_kwd = input_value;
-		if (search_cb) {
-			search_cb(input_value);
-		}
-	}
-}
-
-function create_filters(preset_filters = new Set()) {
+function create_filters(checked_set, search_params) {
 	return filter_cates.map((cate) => ({
 		prop: cate.prop,
 		title: cate.title,
 		multi: cate.multi,
 		toggleable: cate.toggleable,
 
+		// is_cap: search_params.get(gen_op_key(cate.prop)) === 'and',
+
 		options: cate.values.map((key, index) => ({
 			key,
-			checked: preset_filters.has(`${cate.prop}.${key}`),
 			icon: cate.icons?.[index],
+			checked: checked_set.has(`${cate.prop}.${key}`),
 		})),
 	}));
-};
-
-function get_qs() {
-	let qs;
-	if (browser) {
-		qs = new URLSearchParams(location.search);
-	}
-	if (!qs) {
-		return [];
-	}
-	return [...qs].map((i) => i[1].split('|').map((key) => `${i[0]}.${key}`)).flat();
 }
 
-function gen_not_selector(selector_str) {
-	return `${item_class}:not(${selector_str})`;
+function get_checked_set(search_params) {
+	return new Set(
+		[...search_params]
+			.filter(([key]) => !key.endsWith('_op'))
+			.filter(([key]) => key !== QUERY_PARAM)
+			.flatMap(([prop, values]) =>
+				values
+					.split('|')
+					.filter(Boolean)
+					.map((value) => `${prop}.${value}`),
+			),
+	);
 }
 
-function gen_filter_style(_filters) {
-	let selectors = _filters
-		.map((cate) => {
-			let checked_props = cate.options
-				.map((option) => {
-					if (!option.checked) {
-						return null;
-					}
-
-					return {
-						prop: cate.prop,
-						value: option.key,
-					};
-				})
-				.filter(Boolean);
-
-			// skip logic, when all un-checked prop's checkbox
-			if (!checked_props.length) {
-				return '';
-			}
-
-			// ? intersection set rule, group later
-			if (cate.multi && cate.is_cap) {
-				return checked_props.map((i) =>
-					gen_not_selector(gen_selector(i.prop, i.value, cate.multi)),
-				);
-			}
-
-			// : union set rule, group same-prop selectors first
-			return gen_not_selector(
-				checked_props.map((i) => gen_selector(i.prop, i.value, cate.multi)).join(),
-			);
-		})
-		.flat()
-		.filter(Boolean)
-		.join();
-
-	return selectors ? `${selectors} { display: none; }` : '';
+function gen_op_key(prop) {
+	return `${prop}_op`;
 }
 
-function gen_search_style(_kwd) {
-	if (!_kwd) {
-		return '';
-	}
-	return `${item_class}:not([data-search*="${_kwd}"]) { display:none; }`;
+function update_params(callback, replace = false) {
+	const params = new URLSearchParams(search_params);
+
+	callback(params);
+
+	goto(`?${params.toString()}`, {
+		replaceState: replace,
+		noScroll: true,
+		keepFocus: true,
+	});
+}
+
+function toggle_filter(prop, value) {
+	update_params((params) => {
+		const values = new Set(
+			(params.get(prop) || '')
+				.split('|')
+				.filter(Boolean),
+		);
+
+		if (values.has(value)) {
+			values.delete(value);
+		}
+		else {
+			values.add(value);
+		}
+
+		if (values.size) {
+			params.set(prop, [...values].join('|'));
+		}
+		else {
+			params.delete(prop);
+		}
+	}, false);
+}
+
+function set_filter_op(prop) {
+	update_params((params) => {
+		const key = `${prop}_op`;
+
+		if (params.get(key) === 'and') {
+			params.delete(key);
+		}
+		else {
+			params.set(key, 'and');
+		}
+	});
+}
+
+function update_search(value) {
+	update_params((params) => {
+		if (value) {
+			params.set(QUERY_PARAM, value);
+		}
+		else {
+			params.delete(QUERY_PARAM);
+		}
+	}, true);
 }
 
 function reset_filter() {
-	filters = create_filters();
-	input_value = '';
+	goto($page.url.pathname, {
+		replaceState: true,
+		noScroll: true,
+	});
 }
 
 function compositionstart() {
 	is_composing = true;
 }
 
-function compositionend() {
+function compositionend(e) {
 	is_composing = false;
+	update_search(e.currentTarget.value);
 }
 
-let is_submited = false;
-function allow_submit_next_time(e) {
-	if (!is_submited) {
-		e.preventDefault();
-		is_submited = true;
-		return;
-	}
+function gen_not_selector(selector_str) {
+	return `${item_class}:not(${selector_str})`;
+}
+
+function gen_filter_style(_filters, search_params) {
+	let selectors = _filters
+		.map((cate) => {
+			let checked_props = cate.options
+				.filter((option) => option.checked)
+				.map((option) => ({
+					prop: cate.prop,
+					value: option.key,
+				}));
+
+			if (!checked_props.length) {
+				return '';
+			}
+
+			if (cate.multi && is_and(cate.prop)) {
+				return checked_props.map((i) =>
+					gen_not_selector(
+						gen_selector(i.prop, i.value, cate.multi),
+					),
+				);
+			}
+
+			return gen_not_selector(
+				checked_props
+					.map((i) =>
+						gen_selector(i.prop, i.value, cate.multi),
+					)
+					.join(),
+			);
+		})
+		.flat()
+		.filter(Boolean)
+		.join();
+
+	return selectors
+		? `${selectors} { display:none; }`
+		: '';
+}
+
+function gen_search_style(_kwd) {
+	return `${item_class}:not([data-search*="${_kwd}"]) { display:none; }`;
+}
+
+function is_and(prop) {
+	return search_params.get(`${prop}_op`) === 'and';
 }
 </script>
 
-<form class="filters" on:reset|preventDefault={reset_filter} on:submit={allow_submit_next_time}>
+
+<form class="filters" on:reset|preventDefault={reset_filter}>
 	<div class="filter input-box">
 		<input
 			type="search"
-			name={query_param}
-			{placeholder}
-			bind:value={input_value}
+			name={QUERY_PARAM}
+			placeholder={placeholder}
+			value={search_kwd}
+			on:input={(e) => !is_composing && update_search(e.currentTarget.value)}
 			on:compositionstart={compositionstart}
 			on:compositionend={compositionend}
 		/>
@@ -237,17 +273,23 @@ function allow_submit_next_time(e) {
 						class="filter-switcher"
 						type="checkbox"
 						switch
-						bind:checked={filter.is_cap}
+						checked={is_and(filter.prop)}
+						on:change={() => set_filter_op(filter.prop)}
 						data-active="∩"
 						data-inactive="∪"
-						title={filter.is_cap ? '聯集 [交集]' : '[聯集] 交集'}
+						title={is_and(filter.prop) ? '聯集 [交集]' : '[聯集] 交集'}
 					/>
 				{/if}
 			</svelte:element>
 
 			{#each filter.options as option (option.key)}
 				<label class="filer-option-label" hidden={!option.key}>
-					<input type="checkbox" bind:checked={option.checked} title={option.key} />
+					<input
+						type="checkbox"
+						checked={option.checked}
+						on:change={() => toggle_filter(filter.prop, option.key)}
+						title={option.key}
+					/>
 
 					{option.key}
 
@@ -263,6 +305,9 @@ function allow_submit_next_time(e) {
 		<svelte:element this="style">{filter_style}{search_style}</svelte:element>
 	</div>
 </form>
+
+
+
 
 
 <style>
