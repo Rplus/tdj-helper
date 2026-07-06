2625ab2c595e9335ef40a8b8e70f80198dfa4ed5
Rplus <rplus.tw@gmail.com>
2026-07-06 23:11:21 +0800

    feat: change data to use jsdelivr cdn, and add a commit hash version controller

--- src/lib/fetch-data.js
+++ src/lib/fetch-data.js
@@ -1,5 +1,6 @@
 import { browser } from '$app/environment';
 import { error } from '@sveltejs/kit';
+import { folder_commit_sha, } from '$lib/data/version.json';
 
 export const cache = new Map();
 
@@ -117,18 +118,18 @@ export function get_role_url_without_proxy(name, lang) {
 }
 
 // const JSON_PATH = 'https://raw.githubusercontent.com/Rplus/tdj-helper/refs/heads/sveltekit/task/rawdata/tdj';
-const JSON_PATH = 'https://raw.githubusercontent.com/Rplus/tdj-data/refs/heads/data/_cache/tdj-roles';
+// const JSON_PATH = 'https://raw.githubusercontent.com/Rplus/tdj-data/refs/heads/data/_cache/tdj-roles';
+const JSON_PATH = 'https://cdn.jsdelivr.net/gh/Rplus/tdj-data@data/_cache/tdj-roles';
 
 export function get_role_url_from_git(role) {
 	let _fn = role.pinyin_tw ? `${role.name}.tw.json` : `${role.path}.cn.json`;
 	let path = JSON_PATH;
 
-
 	if (import.meta.env.DEV) {
 		path = 'http://localhost:9527';
 	}
 
-	return `${path}/${_fn}`;
+	return `${path}/${_fn}?version=${folder_commit_sha}`;
 }
 function get_role_url(name, lang) {
 	let _url = get_role_url_without_proxy(name, lang);
