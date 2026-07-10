950c3491ffbc0cf0a05d9316c447f100e8800442
Rplus <rplus.tw@gmail.com>
2026-07-10 21:56:13 +0800

    minor: add title for toggle fullscreen

--- src/routes/role/[slug]/Skin.svelte
+++ src/routes/role/[slug]/Skin.svelte
@@ -89,7 +89,7 @@ async function toggle_fullscreen(event) {
 				{/if}
 			{/key}
 
-			<div class="fullscreen-btn" on:click={(e) => toggle_fullscreen(e)}>
+			<div class="fullscreen-btn" title="切換全螢幕" on:click={(e) => toggle_fullscreen(e)}>
 				<!-- ⛶ -->
 				<svg class="enter-fullscreen-icon" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
 					<path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
