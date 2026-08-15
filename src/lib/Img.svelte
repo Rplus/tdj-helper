a82f44efb2ab09b9715960197135a4aa84fcfc5e
Rplus <rplus.tw@gmail.com>
2026-08-15 12:27:03 +0800

    fix: remove 1st fallback
    due to there is a default param in wsrv service

--- src/lib/Img.svelte
+++ src/lib/Img.svelte
@@ -9,7 +9,7 @@ let img;
 let err_count = 0; // 0: 主圖, 1: 嘗試參數圖, 2: 嘗試 CN 網域備援
 
 function handle_error() {
-	if (err_count > 1) {
+	if (err_count > 0) {
 		return;
 	}
 
@@ -17,31 +17,52 @@ function handle_error() {
 		const parsed_url = new URL(this.src);
 
 		if (err_count === 0) {
-			const url_param = parsed_url.searchParams.get('url');
 			err_count = 1;
+			const qs = parsed_url.searchParams;
+			const url_param = qs.get('url');
+			const is_default = (qs.get('default') === '1');
 
-			// 如果有 url 參數且不是當前網址，先走第一防線
-			if (url_param && url_param !== this.src) {
-				this.src = url_param;
-				return;
+			// console.log(123, is_default);
+
+			if (is_default && url_param) {
+				const tw_path_prefix = '/media/pictures/tdj/';
+				const tw_url = new URL(url_param);
+
+				if (tw_url.pathname.startsWith(tw_path_prefix)) {
+					const cn_domain = 'https://media.zlongame.com';
+					const cn_pathname = tw_url.pathname.replace(
+						tw_path_prefix,
+						'/media/news/cn/tdj/'
+					);
+					// console.log(1123, {cn_pathname});
+
+					this.src = `${cn_domain}${cn_pathname}`;
+					return;
+				}
 			}
-		} else if (err_count === 1) {
-			const tw_path_prefix = '/media/pictures/tdj/';
-
-			if (parsed_url.pathname.startsWith(tw_path_prefix)) {
-				err_count = 2; // 往下推進，準備迎接可能發生的 CN 破圖
-
-				const cn_domain = 'https://media.zlongame.com';
-				const cn_pathname = parsed_url.pathname.replace(
-					tw_path_prefix,
-					'/media/news/cn/tdj/'
-				);
-				console.log(1123, {cn_pathname});
-
-				this.src = `${cn_domain}${cn_pathname}`;
-				return;
-			}
+			// // 如果有 url 參數且不是當前網址，先走第一防線
+			// if (url_param && url_param !== this.src) {
+			// 	// this.src = url_param;
+			// 	return;
+			// }
 		}
+		// else if (err_count === 1) {
+		// 	const tw_path_prefix = '/media/pictures/tdj/';
+
+		// 	if (parsed_url.pathname.startsWith(tw_path_prefix)) {
+		// 		err_count = 2; // 往下推進，準備迎接可能發生的 CN 破圖
+
+		// 		const cn_domain = 'https://media.zlongame.com';
+		// 		const cn_pathname = parsed_url.pathname.replace(
+		// 			tw_path_prefix,
+		// 			'/media/news/cn/tdj/'
+		// 		);
+		// 		console.log(1123, {cn_pathname});
+
+		// 		this.src = `${cn_domain}${cn_pathname}`;
+		// 		return;
+		// 	}
+		// }
 	} catch (e) {
 		console.error('URL 解析失敗', e);
 	}
